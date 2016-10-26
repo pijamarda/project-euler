aea2a49479351bc8caf018817d71492eabeefe44
zeneke <zeneke@gmail.com>
2016-10-26 17:32:59 +0200

    problem 5 solved

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,13 @@ fn main() {
     /*
     problem1();   
     problem2(4000000);
-    */
-    //       600851475143
-    //       124000000   3m 3sec con terminal
-    //       124000000   3sec sin terminal
-    //       40000000   54secs con terminal
-    //       1240000000 25sec sin terminal
-    //       6008514751 25sec sin terminal
     problem3(600851475143);
+    problem4(56);
+    */
+    problem5();
 }
 
+#[allow(dead_code)]
 fn problem1() {    
     let mut sum = 0;
     for i in 1..1000 {
@@ -28,6 +25,7 @@ fn problem1() {
     println!("{}",sum);    
 }
 
+#[allow(dead_code)]
 fn problem2(max: i32) {
     let mut n1 = 1;
     let mut n2 = 2;
@@ -52,6 +50,7 @@ fn problem2(max: i32) {
     The prime factors of 13195 are 5, 7, 13 and 29.
     What is the largest prime factor of the number 600.851.475.143 ?    
 */
+#[allow(dead_code)]
 fn problem3(number_original: u64) {    
     let mut largest: u64 = 0;   
     let mut i:u64 = 2; 
@@ -70,17 +69,59 @@ fn problem3(number_original: u64) {
     println!("The largest prime factor of {} is: {}", number_original, largest);
 }
 
+/*
+    A palindromic number reads the same both ways. 
+    The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+#[allow(dead_code)]
+fn problem4(num: i32) {
+    check_palindrome(num);
+}
+
+/*
+    Smallest multiple
+    Problem 5
+    2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+fn problem5() {
+    let mut found = false;
+    let mut number = 1;
+    let mut i = 1;
+    while !found {        
+        while i <= 20 {
+            if number % i == 0 {                
+                i += 1;                
+            }
+            else {
+                i = 1;
+                number += 1;                
+                continue;
+            }
+        }        
+        found = true;
+        println!("The number is: {}", number);        
+    }
+}
+
+fn check_palindrome(num: i32) -> bool {
+    let mut is_palindrome = false;
+    let num_text = num.to_string();
+    println!("{}", num_text);
+    if is_palindrome {
+        is_palindrome=true;
+    }
+    return is_palindrome;
+}
+
 // Calculates modulo
 fn modulo(x: i32, y: i32) -> i32 {
     return ((x % y) + y) % y;
 }
 
-// Calculates modulo
-fn modulo_u64(x: u64, y: u64) -> u64 {
-    return ((x % y) + y) % y;
-}
-
 // Used to help on problem 2
+#[allow(dead_code)]
 fn fibonacci(max: i32) {
     let mut n1 = 1;
     let mut n2 = 2;
@@ -98,6 +139,7 @@ fn fibonacci(max: i32) {
     println!(""); 
 }
 
+#[allow(dead_code)]
 fn is_prime(number: i32) -> bool {
     let mut prime: bool = true;
     for i in 2..(number - 1) {
@@ -110,7 +152,7 @@ fn is_prime(number: i32) -> bool {
 
 fn is_prime_u64(number: u64) -> bool {
     let mut prime: bool = true;
-    for i in (2..number/2) {
+    for i in 2..number/2 {
         //print!("{} ", i);
         if number % i == 0 {
             prime = false;
